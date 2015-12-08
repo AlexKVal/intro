8c9849f810ca2d35c8dda3448f3d6092d8af3f04
AlexKVal <alexkval@gmail.com>
2015-12-08 14:36:03 +0200

    [added] 'Person' model's definitions

--- api/models/Person.js
+++ api/models/Person.js
@@ -8,6 +8,18 @@
 module.exports = {
 
   attributes: {
-
+    firstName: {
+      type: 'string',
+      size: 128,
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      size: 128
+    },
+    contacts: {
+      collection: 'Contact',
+      via: 'person'
+    }
   }
 };
