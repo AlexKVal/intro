238bc0f3e296d9508b24cd3359342c2edd546f54
AlexKVal <alexkval@gmail.com>
2015-12-09 17:40:35 +0200

    [added] a custom 'findOne' handler for 'Person' model

--- api/controllers/PersonController.js
+++ api/controllers/PersonController.js
@@ -1,3 +1,9 @@
 module.exports = {
-
+  findOne: function(req, res) {
+    Person.find(req.params.id).exec(function(err, persons) {
+      var person = persons[0];
+      person.fullName = person.firstName + ' ' + person.lastName;
+      res.json(person);
+    })
+  }
 }
